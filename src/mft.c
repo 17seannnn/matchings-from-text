83506cb58f454a09294fd6dc499fd3e2c0217258
17seannnn <hoggarthlife@gmail.com>
2021-06-11 17:33:19 +0300

    Use is_ln and is_eof, if res == 1 then we can compare, some changes in README and show_help()

--- src/mft.c
+++ src/mft.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 
 enum { patterns_buffer_size = 32, word_buffer_size = 256,
-       star_replace = 1, question_replace = 2, empty_replace = 3,
-       status_read = 1, status_readln = 2, status_ln = 0, status_readEOF = 3 };
+       star_replace = 1, question_replace = 2, empty_replace = 3 };
 
 int string_length(const char *str)
 {
@@ -64,7 +63,7 @@ int match(const char *str, const char *pat)
     }
 }
 
-int catch(char *word, int *line, int *pos)
+int catch(char *word, int *is_ln, int *is_eof, int *line, int *pos)
 {
 /* Skip empty spaces and process linebreak and stop statuses */
     int c, i;
@@ -75,9 +74,11 @@ int catch(char *word, int *line, int *pos)
                 ++*pos;
                 break;
             case '\n':
-                return status_ln;
+                *is_ln = 1;
+                return 0;
             case EOF:
-                return EOF;
+                *is_eof = 1;
+                return 0;
         }
     }
 /* Catch a word */
@@ -86,18 +87,16 @@ int catch(char *word, int *line, int *pos)
         c = getchar();
     }
     word[i] = 0;
-    switch(c) {
-        case '\n':
-            return status_readln;
-        case EOF:
-            return status_readEOF;
-    }
-    return status_read;
+    if(c == '\n')
+        *is_ln = 1;
+    else if(c == EOF)
+        *is_eof = 1;
+    return 1;
 }
 
 void preprocess(char *pat)
 {
-/* Set '*' for better search */
+/* Add '*' to the beginning for better search */
     int len = string_length(pat);
     pat[len+1] = 0;
     for(; len > 0; len--)
@@ -122,16 +121,17 @@ void preprocess(char *pat)
 void show_help()
 {
     printf("\
-Usage: mft \"-param\" \"[Pattern1]\" \"[Pattern2]\"...\n\n\
-Params: -q || --quiet = show matches without line and position\n\
-        --help = show help\n\n\
+Usage: mft -[PARAM] '[Pattern1]' '[Pattern2]'...\n\n\
+Params: -q, --quiet = show matches without line and position\n\
+        --help      = show help\n\n\
 Special characters for patterns:\n\
         '*' = any characters of any length\n\
         '?' = any character, but can not be empty\n\
-        '\\' = place it before *, ? and \\ for finding star,\n\
-                                           question mark and backslash\n\
+        '\\' = place it before * and ? for finding star and question mark\n\
 Examples:\n\
-\"?orem\" \"b*ye\" \"questions\\?\" \"\\*stars\\*\" \"\\\\\"\n\n");
+'?orem' 'b*ye' 'questions\\?' '\\*stars\\*' '\\'\n\n\
+If you find bugs - hoggarthlife@gmail.com\n\
+Author: https://github.com/17sean\n");
     exit(0);
 }
 
@@ -141,7 +141,7 @@ int main(int argc, char **argv)
         fprintf(stderr, "Wrong count of parameters\n");
         return 1;
     }
-    int i, k, res, quiet, pat_count, line, pos;
+    int i, k, res, is_ln, is_eof, quiet, pat_count, line, pos;
 /* Check a param */
     quiet = 0;
     if(string_compare(argv[1], "--help"))
@@ -149,6 +149,7 @@ int main(int argc, char **argv)
     else if(string_compare(argv[1], "-q") ||
             string_compare(argv[1], "--quiet"))
         quiet = 1;
+/* Allocate mem */
     char **pat = malloc(sizeof(char*)*patterns_buffer_size);
     for(i = 0; i < patterns_buffer_size; i++)
         pat[i] = malloc(sizeof(char)*word_buffer_size);
@@ -161,9 +162,14 @@ int main(int argc, char **argv)
     }
     pat_count = i;
     line = pos = 1;
-    for(;;) { /* todo make bools */
-        res = catch(word, &line, &pos);
-        if(res > 0) {
+    is_ln = is_eof = 0;
+    while(!is_eof) {
+        res = catch(word, &is_ln, &is_eof, &line, &pos);
+        /*
+         * If res == 1 then we
+         * can compare a word
+         */
+        if(res) {
             for(i = 0; i < pat_count; i++) {
                 if(match(word, pat[i])) {
                     if(quiet)
@@ -173,14 +179,13 @@ int main(int argc, char **argv)
                     break;
                 }
             }
-            pos += string_length(word) + 1; /* + 1 add space pos */
+            pos += string_length(word) + 1; /* + 1 for space and tab */
         }
-        if(res == status_readln || res == status_ln) {
+        if(is_ln) {
             line++;
             pos = 1;
+            is_ln = 0;
         }
-        if(res == status_readEOF || res == EOF)
-            break;
     }
     for(i = 0; i < patterns_buffer_size; i++)
         free(pat[i]);
