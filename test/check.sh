c1b740ad33a2d66a3bbaa0a0799f0e3a806915c5
17seannnn <hoggarthlife@gmail.com>
2021-06-12 19:52:55 +0300

    Move check.sh into test

--- test/check.sh
+++ test/check.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
 
 # compile
-gcc -Wall -g mft.c -o mft
+gcc -Wall -g ../src/mft.c -o ../src/mft
 echo ""
 
 # With -q
 echo "PARAMTEST -q"
-cat ../test/linepos.txt | ./mft -q '?orem'
+cat ../test/linepos.txt | ./../src/mft -q '?orem'
 echo ""
 
 # With --quiet
 echo "PARAMTEST --quiet"
-cat ../test/linepos.txt | ./mft --quiet '?orem'
+cat ../test/linepos.txt | ./../src/mft --quiet '?orem'
 echo ""
 
 # With --help
 echo "PARAMTEST --help"
-./mft --help
+./../src/mft --help
 echo ""
 
 # Check backslash  
 echo "CHARACTERSTEST"
-cat ../test/linepos.txt | ./mft '\?' '\*' '\'
+cat ../test/linepos.txt | ./../src/mft '\?' '\*' '\'
 echo ""
 
 # Check full text
 echo "FULLTEXTTEST"
-cat ../test/linepos.txt | ./mft '*'
+cat ../test/linepos.txt | ./../src/mft '*'
 echo ""
 
 # Check lorem
 echo "LOREMTEST"
-cat ../test/text.txt | ./mft 'lorem'
+cat ../test/text.txt | ./../src/mft 'lorem'
 echo ""
