b6a1c1ad307cecaccda341ef7b29104d287149e8
17seannnn <17sean@protonmail.com>
2021-06-23 21:10:56 +0300

    test: upgrade check.sh

--- test/check.sh
+++ test/check.sh
@@ -4,32 +4,57 @@
 gcc -Wall -g ../src/main.c -o ../src/mft
 echo ""
 
-# With -q
-echo "PARAMTEST -q"
-cat ../test/linepos.txt | ./../src/mft -q '?orem'
-echo ""
-
-# With --quiet
-echo "PARAMTEST --quiet"
-cat ../test/linepos.txt | ./../src/mft --quiet '?orem'
-echo ""
-
 # With --help
 echo "PARAMTEST --help"
 ./../src/mft --help
 echo ""
 
-# Check backslash  
-echo "CHARACTERSTEST"
-cat ../test/linepos.txt | ./../src/mft '\?' '\*' '\'
+# With -h
+echo "PARAMTEST -h"
+./../src/mft -h
+echo ""
+
+# With -q
+echo "PARAMTEST -q"
+cat linepos.txt | ./../src/mft -q '?orem'
+echo ""
+
+# With --quiet
+echo "PARAMTEST --quiet"
+cat linepos.txt | ./../src/mft --quiet '?orem'
+echo ""
+
+# With -c
+echo "PARAMTEST -c"
+cat any_cases.txt | ./../src/mft -c '?orem'
+echo ""
+
+# With --any-cases
+echo "PARAMTEST --any-cases"
+cat any_cases.txt | ./../src/mft --any-cases '?orem'
+echo ""
+
+# With -f and -p
+echo "PARAMTEST -f -p"
+./../src/mft -f text.txt -p 'lorem'
+echo ""
+
+# With --file and --pattern
+echo "PARAMTEST --file --pattern"
+./../src/mft --file text.txt --pattern 'lorem'
+echo ""
+
+# Check backslash
+echo "CHARACTERSTEST \\"
+./../src/mft -f linepos.txt -p '\?' '\*' '\'
 echo ""
 
 # Check full text
 echo "FULLTEXTTEST"
-cat ../test/linepos.txt | ./../src/mft '*'
+./../src/mft -f linepos.txt -p '*'
 echo ""
 
 # Check lorem
 echo "LOREMTEST"
-cat ../test/text.txt | ./../src/mft 'lorem'
+./../src/mft -f text.txt -p 'lorem'
 echo ""
